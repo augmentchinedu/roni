42450f0a7798b5c98758faddd0299401bacc1cff
augmentchinedu <augmentchinedu@gmail.com>
2026-01-23 04:56:18 +0000

    Vue Router Installed

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
+  <h1>Welcome to {{ appName }}!</h1>
   <RouterView />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      appName: PROJECT.id,
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: #4caf50;
+  font-family: sans-serif;
+}
+</style>
