d7b13430be1ad2f83a02a4cf5650a686ac10e4e5
augmentchinedu <augmentchinedu@gmail.com>
2026-02-24 12:04:06 +0000

    Builds Roni

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
   channel = "stable-24.05";
 
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs_22
     pkgs.yarn
   ];
 
@@ -12,7 +12,6 @@
 
   idx = {
     extensions = [
-      "google.gemini-cli-vscode-ide-companion"
     ];
 
     workspace = {
