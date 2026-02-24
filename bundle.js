31ddc5148b75e4a6d107f0844360862865b5493f
augmentchinedu <augmentchinedu@gmail.com>
2026-02-24 13:00:51 +0000

    Builds Roni

---
+++ bundle.js
@@ -0,0 +1,1 @@
+console.log('Roni Initialized');
---
+++ rollup.config.mjs
@@ -0,0 +1,9 @@
+// rollup.config.mjs
+export default {
+  input: "main.js", // your entry file
+  output: {
+    file: "bundle.js", // the output file for SEA
+    format: "esm", // keep it as ESM
+    sourcemap: false,
+  },
+};
