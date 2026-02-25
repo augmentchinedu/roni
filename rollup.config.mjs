e7f4295101de43cf19b2f929c79053fc7d68ddd2
augmentchinedu <augmentchinedu@gmail.com>
2026-02-25 10:29:39 +0000

    Build runs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'index.js',
+  input: 'kernel/boot.js',
   output: {
     file: 'bundle.cjs',      // .cjs extension bypasses "type": "module"
     format: 'cjs',
